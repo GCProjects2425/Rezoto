f7c8f2d1f56b05ee3f524aed09da824dc11cb846
GenoWeb <49623340+EvilBrain@users.noreply.github.com>
2025-02-05 14:11:30 +0100

    SZVBZUVZEF

--- Rezoto/UDPClient.cpp
+++ Rezoto/UDPClient.cpp
@@ -38,7 +38,7 @@ void UDPClient::Connect(std::string ip, int port)
     // setup address structure
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
-    server.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+    server.sin_addr.s_addr = inet_addr(ip.c_str());
     if (connect(client_socket, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR)
     {
         std::cout << "connect() failed with error code: " << WSAGetLastError() << "\n";
@@ -55,25 +55,40 @@ void UDPClient::Run()
     while (true) {
         if (m_isConnected)
         {
-			const char* message = "Hello from client!";
-            if (sendto(client_socket, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR) {
-                std::cout << "sendto() failed with error code: " << WSAGetLastError() << "\n";
-                exit(EXIT_FAILURE);
-            }
+			//const char* message = "Hello from client!";
+   //         if (sendto(client_socket, message, strlen(message), 0, (sockaddr*)&server, sizeof(sockaddr_in)) == SOCKET_ERROR) {
+   //             std::cout << "sendto() failed with error code: " << WSAGetLastError() << "\n";
+   //             exit(EXIT_FAILURE);
+   //         }
 
-            int answer_length = recvfrom(client_socket, answer, BUFLEN, 0, (sockaddr*)&server, &slen);
-            if (answer_length == SOCKET_ERROR) {
+            //int answer_length = recvfrom(client_socket, answer, BUFLEN, 0, (sockaddr*)&server, &slen);
+            //if (answer_length == SOCKET_ERROR) {
+            //    int errorCode = WSAGetLastError();
+            //    if (errorCode == WSAEWOULDBLOCK) {
+            //        continue; // Pas de données, on continue à écouter
+            //    }
+            //    std::cout << "recvfrom() failed with error code: " << errorCode << "\n";
+            //    break;
+            //}
+
+
+            //answer[answer_length] = '\0'; // Assure que le message est bien terminé
+            //std::cout << "Server: " << answer << "\n";
+
+            int message_len;
+            int slen = sizeof(sockaddr_in);
+            if ((message_len = recvfrom(client_socket, answer, BUFLEN, 0, (sockaddr*)&server, &slen)) == SOCKET_ERROR) {
                 int errorCode = WSAGetLastError();
-                if (errorCode == WSAEWOULDBLOCK) {
-                    continue; // Pas de données, on continue à écouter
+                //cout << "recvfrom() failed with error code: " << errorCode << "\n";
+                if (errorCode == WSAECONNRESET) {
+                    std::cout << "Client forcibly closed the connection.\n";
                 }
-                std::cout << "recvfrom() failed with error code: " << errorCode << "\n";
-                break;
+                continue;  // Continue la boucle au lieu d'exécuter `exit(0)`
             }
 
-
-            answer[answer_length] = '\0'; // Assure que le message est bien terminé
-            std::cout << "Server: " << answer << "\n";
+            // print details of the client/peer and the data received
+            std::cout << "Received packet from " << inet_ntoa(server.sin_addr) << " " << ntohs(server.sin_port) << "\n";
+            std::cout << "Data: " << answer << "\n";
         }
     }
 }
