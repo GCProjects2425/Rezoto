1a909676de692fb4c73c45c4cad23ae7d8500d24
GenoWeb <49623340+EvilBrain@users.noreply.github.com>
2025-02-05 12:27:56 +0100

    les threads ont des noms bro

--- RezotoServer/RezotoServer.cpp
+++ RezotoServer/RezotoServer.cpp
@@ -15,9 +15,10 @@ int main() {
     HINSTANCE hInstance = GetModuleHandle(NULL);
     GameServerGUI gui(hInstance);
     std::thread guiThread(std::bind(&GameServerGUI::Run, &gui));
+    SetThreadDescription((HANDLE)guiThread.native_handle(), L"GuiThread");
 
-    UDPServer udpServer;
-    std::thread networkThread(std::bind(&UDPServer::Start, &udpServer));
+    std::thread networkThread(std::bind(&UDPServer::Start, UDPServer::GetInstance()));
+    SetThreadDescription((HANDLE)networkThread.native_handle(), L"NetworkThread");
 
     networkThread.join();
     guiThread.join();
